5fab0b2b8af5e563f11a05f0606fb69e9904b6d2
erzaikhsan <ikhsanierza@gmail.com>
2023-09-17 14:27:00 +0700

    feat: tambah rumus keliling segitiga

--- challenge.js
+++ challenge.js
@@ -15,9 +15,12 @@ class Segitiga {
 
 const segitigaA = new Segitiga(10, 5);
 const segitigaB = new Segitiga(20, 7);
+const segitigaC = new Segitiga(30, 10);
 
 console.log(segitigaA.hitungLuas());
+console.log(segitigaB.hitungLuas());
 console.log(segitigaB.hitungLuas());
 
 console.log(segitigaA.hitungKeliling(10, 10));
 console.log(segitigaB.hitungKeliling(20, 20));
+console.log(segitigaB.hitungKeliling(20, 25));
