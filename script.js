42453aa9112ef619c17e69341fd9f562dc168506
Moniedelli <109639068+Moniedelli@users.noreply.github.com>
2023-09-17 15:28:42 +0800

    Add files via upload
    menambah code trapesium

---
+++ script.js
@@ -0,0 +1,23 @@
+//trapesium
+
+class Trapesium {
+  constructor(atas, bawah, tinggi, sisiKiri, sisiKanan) {
+    this.atas = atas;
+    this.bawah = bawah;
+    this.tinggi = tinggi;
+    this.sisiKiri = sisiKiri;
+    this.sisiKanan = sisiKanan;
+  }
+
+  kelilingTrapesium() {
+    return this.atas + this.bawah + this.sisiKiri + this.sisiKanan;
+  }
+
+  luasTrapesium() {
+    return ((this.atas + this.bawah) * this.tinggi) / 2;
+  }
+}
+
+const trapesium = new Trapesium(6, 8, 5, 4, 4);
+console.log('keliling Trapesium:', trapezium.kelilingTrapesium());
+console.log('luas Trapesium:', trapezium.luasTrapesium());
