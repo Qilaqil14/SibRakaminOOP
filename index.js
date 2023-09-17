1e989286cf645d8b379d3094476dc4110f158d24
Mukhamad Sa'id Aqil <142391294+Qilaqil14@users.noreply.github.com>
2023-09-17 15:07:03 +0700

    lingkaran

--- index.js
+++ index.js
@@ -41,3 +41,38 @@ console.log(`Luas Segitiga : ${segitigaC.hitungLuas()}`);
 console.log(`Keliling Segitiga : ${segitigaA.hitungKeliling(10, 10)}`);
 console.log(`Keliling Segitiga : ${segitigaA.hitungKeliling(20, 20)}`);
 console.log(`Keliling Segitiga : ${segitigaA.hitungKeliling(30, 20)}`);
+
+
+class Lingkaran {
+  constructor(jariJari) {
+    this.jariJari = jariJari;
+  }
+
+  hitungKeliling() {
+    const keliling = 2 * Math.PI * this.jariJari; //Rumus keliling lingkaran = 2 x Phi x r
+    return keliling;
+  }
+
+  hitungLuas() {
+    const luas = Math.PI * Math.pow(this.jariJari, 2); //Rumus luas lingkaran = phi x r2
+    return luas;
+  }
+}
+
+// Contoh penggunaan
+const jariJariLingkaran = 5;
+const lingkaran = new Lingkaran(jariJariLingkaran);
+
+const keliling = lingkaran.hitungKeliling();
+const luas = lingkaran.hitungLuas();
+
+console.log(
+  `Keliling lingkaran dengan jari-jari ${jariJariLingkaran} adalah ${keliling.toFixed(
+    2
+  )}`
+);
+console.log(
+  `Luas lingkaran dengan jari-jari ${jariJariLingkaran} adalah ${luas.toFixed(
+    2
+  )}`
+);
